c97d9fcd3e323a242a9689e1b022322767fddc88
antariksha-git <heyantariksha@gmail.com>
2024-12-16 20:49:04 +0530

    Added missing annotations

--- src/main/java/com/numeroalpha/pma/exception/PersonNotFoundException.java
+++ src/main/java/com/numeroalpha/pma/exception/PersonNotFoundException.java
@@ -1,8 +1,12 @@
 package com.numeroalpha.pma.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class PersonNotFoundException extends RuntimeException {
 	private int status;
 	private String message;
