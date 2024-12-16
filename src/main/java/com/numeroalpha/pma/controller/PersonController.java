30fb75c3c111a129be2c7378478919fa057cbbc7
antariksha-git <heyantariksha@gmail.com>
2024-12-16 22:14:26 +0530

    Refactored response with proper response structure

--- src/main/java/com/numeroalpha/pma/controller/PersonController.java
+++ src/main/java/com/numeroalpha/pma/controller/PersonController.java
@@ -1,16 +1,18 @@
 package com.numeroalpha.pma.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.numeroalpha.pma.entity.Person;
 import com.numeroalpha.pma.requestdto.PersonRequest;
 import com.numeroalpha.pma.responsedto.PersonResponse;
+import com.numeroalpha.pma.responsestructure.AppResponseStructure;
+import com.numeroalpha.pma.responsestructure.ResponseStructure;
 import com.numeroalpha.pma.service.PersonService;
 
 import lombok.AllArgsConstructor;
@@ -23,13 +25,17 @@ public class PersonController {
 	private final PersonService personService;
 	
 	@PostMapping
-	public PersonResponse createPerson(@RequestBody PersonRequest personRequest) {
-		return personService.createPerson(personRequest);
+	public ResponseEntity<ResponseStructure<PersonResponse>> createPerson(@RequestBody PersonRequest personRequest) {
+		return AppResponseStructure
+				.getResponseEntity(HttpStatus.CREATED, 
+						new ResponseStructure<>(201, "User created successfully!", personService.createPerson(personRequest)));
 	}
 	
 	@GetMapping("/{personId}")
-	public PersonResponse getPersonById(@PathVariable int personId) {
-		return personService.getPersonById(personId);
+	public ResponseEntity<ResponseStructure<PersonResponse>> getPersonById(@PathVariable int personId) {
+		return AppResponseStructure
+				.getResponseEntity(HttpStatus.OK, 
+						new ResponseStructure<>(200, "User found Successfully!", personService.getPersonById(personId)));
 	}
 
 }
