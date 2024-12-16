90e65d1e53bd2d2f0577371618371519e8cc5227
antariksha-git <heyantariksha@gmail.com>
2024-12-16 19:44:42 +0530

    Refactored subpackage structure

--- src/main/java/com/numeroalpha/pma/PersonService.java
+++
@@ -1,5 +0,0 @@
-package com.numeroalpha.pma;
-
-public class PersonService {
-
-}
---
+++ src/main/java/com/numeroalpha/pma/service/PersonService.java
@@ -0,0 +1,20 @@
+package com.numeroalpha.pma.service;
+
+import org.springframework.stereotype.Service;
+
+import com.numeroalpha.pma.entity.Person;
+import com.numeroalpha.pma.repository.PersonRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class PersonService {
+	
+	private final PersonRepository personRepository;
+	
+	public Person createPerson(Person person) {
+		return personRepository.save(person);
+	}
+
+}
