71b763b83d5ba384809b4d6d84b4fc830fdb54fe
antariksha-git <heyantariksha@gmail.com>
2024-12-16 19:23:09 +0530

    first commit

---
+++ src/test/java/com/numeroalpha/pma/PersonManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numeroalpha.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
