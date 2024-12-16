635ae3f350ec073e8951584e88db85ccb8f8fd4c
antariksha-git <heyantariksha@gmail.com>
2024-12-16 19:59:55 +0530

    Added PersonRequest dto

--- src/main/java/com/numeroalpha/pma/controller/PersonController.java
+++ src/main/java/com/numeroalpha/pma/controller/PersonController.java
@@ -1,5 +1,7 @@
 package com.numeroalpha.pma.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,11 @@ public class PersonController {
 	@PostMapping
 	public Person createPerson(@RequestBody Person person) {
 		return personService.createPerson(person);
+	}
+	
+	@GetMapping("/{personId}")
+	public Person getPersonById(@PathVariable int personId) {
+		return personService.getPersonById(personId);
 	}
 
 }
---
+++ src/main/java/com/numeroalpha/pma/requestdto/PersonRequest.java
@@ -0,0 +1,11 @@
+package com.numeroalpha.pma.requestdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonRequest {
+	private String name;
+	private int age;
+}
